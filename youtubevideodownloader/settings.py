56a55536b474125effdbad19068dcbaee39a9c8f
Yash-Programmer <yash.gurukul12@gmail.com>
2023-04-07 11:50:55 +0530

    more

--- youtubevideodownloader/settings.py
+++ youtubevideodownloader/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-q0k3&6ji0@!34cg4&sk0+lh$_!9zwrt4#cdx2lb3p22onm*tgq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '*']
 
 
 # Application definition
@@ -123,5 +124,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [BASE_DIR / 'static'] 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT =  BASE_DIR / 'media' 
 MEDIA_URL = '/media/' 
